38a8f4f1498cfd522704b9d89a75a4e93a3bed49
ifaint <dzhuang@scut.edu.cn>
2021-01-19 03:57:04 +0800

    create superuser in migration.

--- latex2pdf/latex/migrations/0001_initial.py
+++ latex2pdf/latex/migrations/0001_initial.py
@@ -9,6 +9,38 @@ import jsonfield.fields
 import latex.models
 import re
 
+import re
+from django.conf import settings
+from django.db import migrations, models
+import django.core.validators
+import os
+import django.utils.timezone
+from latex.models import OverwriteStorage
+
+
+def generate_superuser(apps, schema_editor):
+    # https://stackoverflow.com/a/53261943/3437454
+    from django.contrib.auth import get_user_model
+    from django.db import IntegrityError
+    User = get_user_model()  # noqa
+
+    django_superuser_username = os.environ.get('DJANGO_SUPERUSER_USERNAME', None)
+    django_superuser_email = os.environ.get('DJANGO_SUPERUSER_EMAIL', None)
+    django_superuser_password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', None)
+    if not (django_superuser_username and django_superuser_password):
+        print("No superuser created.")
+        return
+
+    try:
+        User.objects.create_superuser(
+            username=django_superuser_username,
+            email=django_superuser_email,
+            password=django_superuser_password)
+    except IntegrityError:
+        print("Superuser with given settings already exists.")
+    else:
+        print("Superuser created.")
+
 
 class Migration(migrations.Migration):
 
@@ -19,6 +51,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(generate_superuser),
         migrations.CreateModel(
             name='LatexCollection',
             fields=[
